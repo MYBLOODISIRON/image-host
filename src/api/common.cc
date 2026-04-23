f248cf4e51b42050bee8027f8b5fbae8472a67ce
Killy <3335655852@qq.com>
2026-04-24 01:47:19 +0800

    模板函数必须放在头文件中

--- src/api/common.cc
+++ src/api/common.cc
@@ -11,16 +11,7 @@ std::string s_shorturl_server_access_token;
 
 
 
-template <typename... Args>
-std::string FormatString(const std::string& format, Args... args)
-{
-    auto size { snprintf(nullptr, 0, format.c_str(), args...) + 1 };
-    std::unique_ptr<char[]> buf {new char [size] };
 
-    snprintf(buf.get(), size, format.c_str(), args...);
-
-    return std::string(buf.get(), buf.get() + size - 1);
-}
 
 
 int TrimSpace(char* inbuf)
--- src/api/include/common.h
+++ src/api/include/common.h
@@ -39,7 +39,15 @@ extern std::string s_shorturl_server_address;
 extern std::string s_shorturl_server_access_token;
 
 template <typename... Args>
-std::string FormatString(const std::string &format, Args... args);
+std::string FormatString(const std::string& format, Args... args)
+{
+    auto size { snprintf(nullptr, 0, format.c_str(), args...) + 1 };
+    std::unique_ptr<char[]> buf {new char [size] };
+
+    snprintf(buf.get(), size, format.c_str(), args...);
+
+    return std::string(buf.get(), buf.get() + size - 1);
+};
 
 
 int TrimSpace(char *inbuf);
